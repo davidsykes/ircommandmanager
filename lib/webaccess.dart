7be60eb35cda101304286452783f692c2418d930
David Sykes <david@sykes.org.uk>
2023-06-29 07:33:00 +0100

    About to try and get data from the web service

---
+++ lib/webaccess.dart
@@ -0,0 +1,41 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class TraceInfo {
+  final String name;
+  final String fileName;
+  final int traceCount;
+  final int traceLength;
+
+  TraceInfo(
+      {required this.name,
+      required this.fileName,
+      required this.traceCount,
+      required this.traceLength});
+}
+
+class WebAccess {
+  static Future<List<TraceInfo>> getPlots() async {
+    // final httpPackageUrl = Uri.https('dart.dev', '/f/packages/http.json');
+    final httpPackageUrl = Uri.parse('http://192.168.1.142:5000/tracenames');
+    final httpPackageInfo = await getP(httpPackageUrl);
+    final decoded = json.decode(httpPackageInfo);
+
+    var traces = List<TraceInfo>.empty(growable: true);
+
+    for (var trace in decoded) {
+      print(trace);
+      traces.add(TraceInfo(
+          name: trace['tracename'],
+          fileName: trace['tracepath'],
+          traceCount: trace['tracecount'],
+          traceLength: trace['tracelength']));
+    }
+
+    return traces;
+  }
+
+  static Future<String> getP(Uri url) async {
+    return http.read(url);
+  }
+}
